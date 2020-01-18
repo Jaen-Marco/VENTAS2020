ae32e015ecdc58adf56ca28db845586e65c4230f
Jaen Idat 408 <jeanmistic08@gmail.com>
2020-01-18 15:38:07 -0500

    Subiendo mi proyecto

---
+++ src/main/java/com/idat/javawebavanzado/repository/product.java
@@ -0,0 +1,10 @@
+package com.idat.javawebavanzado.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.idat.javawebavanzado.model.Product;
+
+public interface product extends CrudRepository<Product, Long>{
+
+
+}
